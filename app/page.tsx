a01f25eb98f6a5f1af99197b5c94603eb7e1288f
Epali Stellae <80137564+epalistellae@users.noreply.github.com>
2023-01-22 17:19:30 -1000

    Bug: MonthPreview is no long async component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import supabase from "../utils/supabase";
 import Image from "next/image";
 
-async function MonthPreview({ month, blur }: { month: string; blur: boolean }) {
+function MonthPreview({
+  month,
+  blur,
+  url,
+}: {
+  month: string;
+  blur: boolean;
+  url: string;
+}) {
   const blurClass = blur ? "blur-xl" : "";
-  const { data } = await supabase.from("fiftytwoweeks").select();
 
   return (
     <div className="w-full px-4 pb-4 md:w-1/2 lg:w-1/4">
@@ -14,7 +21,7 @@ async function MonthPreview({ month, blur }: { month: string; blur: boolean }) {
         <div className="grid grid-cols-2 grid-rows-2 h-full">
           <div className="relative">
             <Image
-              src={data[0].imgSrc}
+              src={url}
               alt="Placeholder"
               fill={true}
               className="object-cover"
@@ -51,22 +58,24 @@ async function MonthPreview({ month, blur }: { month: string; blur: boolean }) {
 }
 
 export default async function Home() {
+  const { data } = await supabase.from("fiftytwoweeks").select();
+  let url = data ? data[0].imgSrc : "";
   return (
     <section className="min-h-screen">
       <div className="container mx-auto px-5 py-10">
         <div className="-m-4 flex flex-wrap">
-          <MonthPreview month="January" blur={false} />
-          <MonthPreview month="February" blur={true} />
-          <MonthPreview month="March" blur={true} />
-          <MonthPreview month="April" blur={true} />
-          <MonthPreview month="May" blur={true} />
-          <MonthPreview month="June" blur={true} />
-          <MonthPreview month="July" blur={true} />
-          <MonthPreview month="August" blur={true} />
-          <MonthPreview month="September" blur={true} />
-          <MonthPreview month="October" blur={true} />
-          <MonthPreview month="November" blur={true} />
-          <MonthPreview month="December" blur={true} />
+          <MonthPreview month="January" blur={false} url={url} />
+          <MonthPreview month="February" blur={true} url="" />
+          <MonthPreview month="March" blur={true} url="" />
+          <MonthPreview month="April" blur={true} url="" />
+          <MonthPreview month="May" blur={true} url="" />
+          <MonthPreview month="June" blur={true} url="" />
+          <MonthPreview month="July" blur={true} url="" />
+          <MonthPreview month="August" blur={true} url="" />
+          <MonthPreview month="September" blur={true} url="" />
+          <MonthPreview month="October" blur={true} url="" />
+          <MonthPreview month="November" blur={true} url="" />
+          <MonthPreview month="December" blur={true} url="" />
         </div>
       </div>
     </section>
